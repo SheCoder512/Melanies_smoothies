7217bbb71f97b6b6861a69a5bf742cf94f9f711b
SheCoder512 <cherylaclise@gmail.com>
2024-06-21 11:52:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,10 @@ ingredients_list = st.multiselect(
 if ingredients_list:
     ingredients_string = ''
 
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+	for fruit_chosen in ingredients_list: ingredients_string += fruit_chosen + ' '
+		st. subheader (fruit_chosen + ' Nutrition Information')
+		fruityvice_response = requests.get ("https://fruityvice.com/api/fruit/" + fruit_chosen)
+		fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -46,8 +48,7 @@ if ingredients_list:
 	   
         st.success('Your Smoothie is ordered!', icon="✅")
 
-# new section to display fruityvice nutrition information
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
 fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=TRUE)
